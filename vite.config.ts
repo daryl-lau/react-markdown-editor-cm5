d7c7d0f0d6a64e3fc4e59a4b1df441a2dbea9e96
liuyi <lyucan_1@163.com>
2023-04-25 02:50:24 +0800

    fix: remove tooltip external

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,6 @@ export default defineConfig({
         'lodash',
         'dayjs',
         'rc-trigger',
-        'rc-tooltip/lib/Tooltip',
         're-resizable',
         'uslug',
         'classnames',
